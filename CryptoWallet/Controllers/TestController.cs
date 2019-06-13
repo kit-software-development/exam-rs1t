7c8d67a9270ce6aa00a22b52b67760e7e06e1c3b
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-06-13 19:39:23 +0300

    Initial commit

---
+++ CryptoWallet/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NBitcoin;
+
+namespace CryptoWallet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("address")]
+        public ActionResult<string> Get(int id)
+        {
+            var privateKey = new Key();
+            var bitcoinSecret = privateKey.GetWif(Network.TestNet);
+            var publicKey = privateKey.PubKey;
+            var bitcoinAddress = publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet);
+            return $"Private key (WIF): {bitcoinSecret}\nAddress: {bitcoinAddress}";
+        }
+    }
+}
